5bd16e5040a629725d75abdcacecf98a398f788d
kojirenan <kj.renan@icloud.com>
2023-12-02 20:26:54 -0300

    adicionando responsividade

--- js/router.js
+++ js/router.js
@@ -1,43 +1,43 @@
 export class Router {
-  routes = {}
+  routes = {};
 
   setObject = {
-    "/": document.querySelector('#home'),
-    "/universe": document.querySelector('#universe'),
-    "/exploration": document.querySelector('#exploration'),
-  }
+    '/': document.querySelector('#home'),
+    '/universe': document.querySelector('#universe'),
+    '/exploration': document.querySelector('#exploration'),
+  };
 
   colorSet() {
-    const { pathname } = window.location
-    const object = this.setObject[pathname]
+    const { pathname } = window.location;
+    const object = this.setObject[pathname];
 
-    this.setObject["/"].removeAttribute('class')
-    this.setObject["/universe"].removeAttribute('class')
-    this.setObject["/exploration"].removeAttribute('class')
-    object.setAttribute('class', 'check')
+    this.setObject['/'].removeAttribute('class');
+    this.setObject['/universe'].removeAttribute('class');
+    this.setObject['/exploration'].removeAttribute('class');
+    object.setAttribute('class', 'check');
   }
 
   add(routeName, page) {
-    this.routes[routeName] = page
+    this.routes[routeName] = page;
   }
 
   route(event) {
-    event = event || window.event
-    event.preventDefault()
+    event = event || window.event;
+    event.preventDefault();
 
-    window.history.pushState({}, "", event.target.href)
+    window.history.pushState({}, '', event.target.href);
 
-    this.handle()
+    this.handle();
   }
 
   handle() {
-    const { pathname } = window.location
-    const route = this.routes[pathname]
+    const { pathname } = window.location;
+    const route = this.routes[pathname];
 
     fetch(route)
       .then(data => data.text())
-      .then(html => document.querySelector('#app').innerHTML = html)
+      .then(html => (document.querySelector('#app').innerHTML = html));
 
-    this.colorSet()
+    this.colorSet();
   }
 }
